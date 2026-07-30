4760a615897452b2ce34fff4aa4b34bc2b73134c
VictorKhuraijam <khuraijamvictor@gmail.com>
2026-07-30 22:37:16 +0530

    minor changes

--- scripts/tmux-checkpoint.sh
+++ scripts/tmux-checkpoint.sh
@@ -1,3 +1,2 @@
-
 #!/bin/bash
-cd ~/.local/share/tmux/resurrect && ln -sf $(/bin/ls -1t tmux_resurrect_* | fzf) last && tmux run-shell ~/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh 
+cd ~/.local/share/tmux/resurrect && ln -sf $(/bin/ls -1t tmux_resurrect_* | fzf) last && tmux run-shell ~/.config/tmux/plugins/tmux-resurrect/scripts/restore.sh
